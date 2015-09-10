556bcd5fa43d4eebc665c7b66b8f63fc7a65bd62
Fireyou <chenguocc@hotmail.com>
2015-09-11 00:19:08 +0200

    完成History模块

--- KeepRunning/History/Controller/CGHistoryTableViewController.m
+++ KeepRunning/History/Controller/CGHistoryTableViewController.m
@@ -7,92 +7,88 @@
 //
 
 #import "CGHistoryTableViewController.h"
+#import "CGSportInfo.h"
+#import "CGReadrawMapViewController.h"
+#import "FMDB.h"
+#import "CGSportInfoSaveTool.h"
 
 @interface CGHistoryTableViewController ()
-
+@property (nonatomic, strong) NSMutableArray *sportInfos;
 @end
 
 @implementation CGHistoryTableViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 #pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return self.sportInfos.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+    
+    NSMutableArray *array = self.sportInfos[section];
+    
+    return array.count;
 }
 
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    NSMutableArray *array = self.sportInfos[section];
+    CGSportInfo *sportInfo = [array lastObject];
     
-    // Configure the cell...
+    
+    return [NSString stringWithFormat:@"%ld",(long)sportInfo.saveID.integerValue];
+    
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    // 1.创建cell
+    static NSString *ID = @"contact";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:ID];
+    }
+    
+    // 2.设置cell的数据
+    CGSportInfo *sportInfo = self.sportInfos[indexPath.section][indexPath.row];
+    NSDateComponents *dateComponent = sportInfo.sportDate;
+    NSInteger year = [dateComponent year];
+    NSInteger month = [dateComponent month];
+    NSInteger day = [dateComponent day];
+    NSInteger hour = [dateComponent hour];
+    NSInteger minute = [dateComponent minute];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld/%02ld/%ld/%ld/%ld",(long)year,(long)month,(long)day,(long)hour,(long)minute];
+    
+    
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%f",[sportInfo.energie floatValue]];
+    
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
     
     return cell;
 }
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+//每次进入window获取最新的数据模型
+- (void)viewDidAppear:(BOOL)animated
+{
+    self.sportInfos = [CGSportInfoSaveTool sportInfos];
+    [self.tableView reloadData];
 }
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    CGReadrawMapViewController *vc = [[CGReadrawMapViewController alloc] init];
+    CGSportInfo *sportInfo = self.sportInfos[indexPath.section][indexPath.row];
+    
+    vc.locations = sportInfo.runLine;
+    
+    [self.navigationController pushViewController:vc animated:YES];
 }
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
