28ec665ed14f82f48aa581eaf6dd29c4fcd0bd18
Fireyou <chenguocc@hotmail.com>
2015-09-10 20:19:09 +0200

    Initial Commit

---
+++ KeepRunning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeepRunning
+//
+//  Created by CHENGuo on 15/9/10.
+//  Copyright (c) 2015年 CHENGuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeepRunning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeepRunning
+//
+//  Created by CHENGuo on 15/9/10.
+//  Copyright (c) 2015年 CHENGuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
